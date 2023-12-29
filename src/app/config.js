8a695fc58a6b6f72285839355237553f9c729e34
HuyTran0996 <tranviquanghuy@gmail.com>
2023-12-29 15:35:55 +0700

    2023-12-29

--- src/app/config.js
+++ src/app/config.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = process.env.REACT_APP_BACKEND_API;
-export const BASE_URL = `http://127.0.0.1:5000/api`;
+export const BASE_URL = `https://project-3-1-backend.onrender.com/api`;
 export const POKEMONS_PER_PAGE = 20;
